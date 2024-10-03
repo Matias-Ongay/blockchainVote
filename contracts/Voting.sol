657a2befb72a933e5612ebb90f7c103c56a80a59
Matias-Ongay <ongaymatias@gmail.com>
2024-10-03 13:32:40 -0300

    contract deployed

---
+++ contracts/Voting.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+    uint public candidatesCount;
+
+    constructor() {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidates[candidatesCount++] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "Already voted.");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate.");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+    }
+
+    function getVoteCount(uint _candidateId) public view returns (uint) {
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate.");
+        return candidates[_candidateId].voteCount;
+    }
+
+    function getCandidatesCount() public view returns (uint) {
+        return candidatesCount;
+    }
+}
